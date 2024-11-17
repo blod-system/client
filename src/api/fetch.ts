88d2aaa93ab03ae9a75d9084fe4c6f383b734559
sin_yu <catmoon1017@gmail.com>
2024-11-18 04:27:47 +0800

    fix: axios instance config

--- src/api/fetch.ts
+++ src/api/fetch.ts
@@ -18,6 +18,10 @@ type ApiResponse<T> = {
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 export async function getApi<T>(params: GetApiParams): Promise<ApiResponse<T>> {
