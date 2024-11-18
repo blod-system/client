1b1bd828c26d4aae2edd5451b145d3584d08d140
sin_yu <catmoon1017@gmail.com>
2024-11-18 08:26:49 +0800

    feat: add log

--- src/pages/Header/index.tsx
+++ src/pages/Header/index.tsx
@@ -82,7 +82,7 @@ export default function Header() {
 			gender: value.gender,
 			is_reminder_active: value.isReminderActive,
 		}
-
+		console.log(params)
 		const updateRes = await updateUserInfo(params)
 		if (updateRes?.status === 200) {
 			messageBox.open({
